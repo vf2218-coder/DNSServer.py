68e21527280d876b91dfec6bdaf25a43b9167324
vf2218-coder <vf2218@nyu.edu>
2026-03-19 22:39:56 -0400

    Handle string input for AES decryption

--- DNSServer.py
+++ DNSServer.py
@@ -40,6 +40,8 @@ def encrypt_with_aes(input_string, password, salt):
 def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
+    if isinstance(encrypted_data, str):
+        encrypted_data = encrypted_data.encode('utf-8')
     decrypted_data = f.decrypt(encrypted_data) #call the Fernet decrypt method
     return decrypted_data.decode('utf-8')
 
